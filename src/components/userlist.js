8071674d1311e55be564c57c583e9a69f29e6e3d
Bruno May <64760466+Mulix27@users.noreply.github.com>
2023-03-11 11:31:30 -0600

    Primer commit

---
+++ src/components/userlist.js
@@ -0,0 +1,39 @@
+// UserList.js
+import React, { Component } from 'react';
+import axios from 'axios';
+import '../style.css';
+
+class UserList extends Component {
+  state = {
+    users: []
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        const users = res.data;
+        this.setState({ users });
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className='title'>User List</h1>
+        <div className="user-list-container">
+          {this.state.users.map(user => (
+            <div className="user-card" key={user.id}>
+              <h2>{user.name}</h2>
+              <p>{user.email}</p>
+              <p>{user.address.city}</p>
+              <p className="company">{user.company.name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default UserList;
+
